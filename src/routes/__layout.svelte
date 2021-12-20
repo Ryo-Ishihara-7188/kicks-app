e9fcae28f110c97d17a010f8a22762ecfc5c6b3d
ishihara <r.funk.7188@gmail.com>
2021-12-20 12:24:54 +0900

    setting tailwind/header

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+	import Header from '$lib/components/Header.svelte';
+</script>
+
+<div class="flex flex-col min-h-screen">
+	<Header />
+	<main class="flex-1 overflow-x-hidden container">
+		<slot />
+	</main>
+	<footer>kicks app</footer>
+</div>
