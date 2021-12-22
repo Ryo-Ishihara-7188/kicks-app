b48aa0315b986adea092d147abce063c1b9d6774
ishihara <r.funk.7188@gmail.com>
2021-12-22 18:24:56 +0900

    auth処理を仮追加

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,62 @@
 	import { onMount } from 'svelte';
 	import { collection, doc, getDoc } from 'firebase/firestore';
 	import { getFunctions, httpsCallable } from 'firebase/functions';
-	import { db } from '$lib/firebase/client.ts';
-	// import firebase from 'firebase/app';
-	// import 'firebase/db';
+	import { db } from '$lib/firebase/client';
+
+	import {
+		getAuth,
+		signInWithPopup,
+		GoogleAuthProvider,
+		signOut,
+		onAuthStateChanged
+	} from 'firebase/auth';
+	import {} from 'firebase/auth';
+
+	function currentUser() {
+		const auth = getAuth();
+		const user = auth.currentUser;
+		console.log(user);
+
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				const uid = user.uid;
+				console.log(uid);
+			}
+		});
+	}
+
+	async function logout() {
+		const auth = getAuth();
+		signOut(auth)
+			.then(() => {
+				// Sign-out successful.
+			})
+			.catch((error) => {
+				// An error happened.
+			});
+	}
+
+	async function login() {
+		const auth = getAuth();
+		const provider = new GoogleAuthProvider();
+		try {
+			const result = await signInWithPopup(auth, provider);
+			const credential = GoogleAuthProvider.credentialFromResult(result);
+			const token = credential.accessToken;
+
+			// The signed-in user info.
+			const user = result.user;
+			console.log(token);
+			console.log(user);
+		} catch (error) {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			// The email of the user's account used.
+			const email = error.email;
+			// The AuthCredential type that was used.
+			const credential = GoogleAuthProvider.credentialFromError(error);
+		}
+	}
 
 	onMount(() => {
 		test();
@@ -12,17 +65,20 @@
 	});
 
 	async function test() {
-		// const db = getFirestore(app);
 		const ref = doc(db, 'test', '1');
 		const res = await getDoc(ref);
 		console.log(res.data());
 	}
 
-	async function testfunc() {
-		const functions = getFunctions();
-		const addMessage = httpsCallable(functions, 'test');
-		await addMessage();
-	}
+	// demo functions
+	// async function testfunc() {
+	// 	const functions = getFunctions();
+	// 	const addMessage = httpsCallable(functions, 'test');
+	// 	await addMessage();
+	// }
 </script>
 
 <h1 class="">Stage</h1>
+<button on:click={login}>ログイン</button>
+<button on:click={logout}>ログアウト</button>
+<button on:click={currentUser}>ユーザー</button>
