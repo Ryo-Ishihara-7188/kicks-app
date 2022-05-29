799c67b6c6ffcd78e27a6ad87b08115d8772e350
ishihara <r.funk.7188@gmail.com>
2022-05-30 03:58:55 +0900

    一旦コミット

--- src/routes/review/add.svelte
+++ src/routes/review/add.svelte
@@ -20,3 +20,8 @@
 </script>
 
 <h1 class="">New Review</h1>
+<img
+	src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA3MiAyNCIgd2lkdGg9IjcyIiBoZWlnaHQ9IjI0IiBkYXRhLXU9Imh0dHBzJTNBJTJGJTJGYW5hdGFub2VuZ2luZWVyLmpwJTJGd3AtY29udGVudCUyRnBsdWdpbnMlMkZzaXRlZ3VhcmQlMkZyZWFsbHktc2ltcGxlLWNhcHRjaGElMkZ0bXAlMkYxOTc2MjE1MzUwLnBuZyIgZGF0YS13PSI3MiIgZGF0YS1oPSIyNCIgZGF0YS1iaXA9IiI+PC9zdmc+"
+	data-spai="1"
+	alt="CAPTCHA"
+/>
